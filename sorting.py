355dc71e8a83dddb0c0c3f2b73c7aca57f54ffec
SakhnoAndrey <Andrey.Sakhno@gmail.com>
2020-01-18 13:52:19 +0200

    Added Mark and Toys

--- sorting.py
+++ sorting.py
@@ -22,5 +22,25 @@ def countSwaps():
     print("First Element: {0}".format(str(a[0])))
     print("Last Element: {0}".format(str(a[-1])))
 
+def maximumToys():
+    # Input maximum toys
+    k = 50
+    s = "1 12 5 111 200 1000 10"
+    prices = list(map(int, s.rstrip().split()))
 
-countSwaps()
+    # Function maximum toys
+    count_max = 0
+    basket = 0
+    prices.sort()
+    print(prices)
+    for p in prices:
+        if basket + p < k:
+            basket += p
+            count_max += 1
+        else:
+            break
+    print(count_max)
+    return count_max
+
+
+maximumToys()
